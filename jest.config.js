fdd50444d61eb701ba4c601b066a950922b68562
dj <odjbin@163.com>
2026-05-28 10:48:20 +0800

    run

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ const path = require('path')
 
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue2-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
       'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
@@ -13,9 +14,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
+  testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)', '**/__tests__/*.(js|jsx|ts|tsx)'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,vue}', '!src/assets/**'],
   coverageReporters: ['html', 'text-summary'],
